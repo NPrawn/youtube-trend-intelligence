bb381401e689e6fab2b8dfa6b9ad46c1ca9bacff
nprawn <ljwljw522@gmail.com>
2026-04-10 15:18:16 +0900

    feat: add youtube api rest client configuration

---
+++ backend/src/main/java/com/jeong/youtubetrend/infra/youtube/YoutubeApiConfig.java
@@ -0,0 +1,15 @@
+package com.jeong.youtubetrend.infra.youtube;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class YoutubeApiConfig {
+    @Bean
+    public RestClient youtubeRestClient(YoutubeApiProperties properties) {
+        return RestClient.builder()
+                .baseUrl(properties.baseUrl())
+                .build();
+    }
+}
